28cf65085bdd94929824c418f9f784b437784f3a
pablo727272 <pablo727272@gmail.com>
2017-08-30 10:47:28 -0600

    added 404

--- server.js
+++ server.js
@@ -3,4 +3,15 @@ var app = express()
 
 app.use(express.static('./public/'))
 
+// 404 page
+app.get('/404', function(req,res){
+    res.sendFile('./html/404.html', {root: './public'})
+})
+
+// 404 error handling middleware
+app.use(function(req, res, next){
+    res.status(404)
+    res.redirect('/404')
+})
+
 app.listen(8080)
